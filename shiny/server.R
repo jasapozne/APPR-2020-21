b86faf1b13f5d4bfe9f7ab5f3b6cf03898cc8c3b
Jaša Pozne <jasa.pozen@student.fmf.uni-lj.si>
2021-02-22 19:55:46 +0100

    dodani zavihki in grafi glede na leta

--- shiny/server.R
+++ shiny/server.R
@@ -1,62 +1,56 @@
 library(shiny)
 library(plotly)
 
+
 shinyServer(function(input, output) {
+  output$IPo <- renderPlotly({izvoz.in.bdp.glede.na.povrsino.shiny %>% filter(leto == input$leto) %>% 
+    ggplot(aes(x=BDP,y=`kolicina_MIO_$`,color=drzava,size=povrsina,label=drzava)) + 
+      geom_point() +
+      ggtitle("Primerjava BDP in izvoza držav s približno enako površino")+  scale_x_log10() + scale_y_log10() + 
+      theme(axis.text.x=element_blank(),
+            axis.ticks.x=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank()) + ylab("količina v mio $")
+  })
+  output$IPr <- renderPlotly({izvoz.in.bdp.glede.na.prebivalce.shiny %>% filter(leto == input$leto) %>% 
+      ggplot(aes(x=BDP,y=`kolicina_MIO_$`,color=drzava,size=populacija,label=drzava)) + 
+      geom_point() +
+      ggtitle("Primerjava BDP in izvoza držav s približno enako prebivalci")+  scale_x_log10() + scale_y_log10() + 
+      theme(axis.text.x=element_blank(),
+            axis.ticks.x=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank()) + ylab("količina v mio $")
+  
+  })
 
-  output$UPr <- renderPlotly({ggplot(uvoz.in.bdp.glede.na.prebivalce.1, aes(x=BDP,y=`kolicina_MIO_$`,color=drzava,size=populacija,label=drzava)) + 
-    geom_point() + geom_text(aes(label=drzava),
-                                                alpha =ifelse(uvoz.in.bdp.glede.na.prebivalce.1$drzava=="Slovenia",1,0)) +
-    ggtitle("Primerjava BDP in uvoza, držav s približno enako populacijo, leta 2019") +  scale_x_log10() + scale_y_log10() +
-    theme(axis.text.x=element_blank(),
-          axis.ticks.x=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) 
+  output$UPo <- renderPlotly({uvoz.in.bdp.glede.na.povrsino.shiny %>% filter(leto == input$leto) %>% 
+      ggplot(aes(x=BDP,y=`kolicina_MIO_$`,color=drzava,size=povrsina,label=drzava)) + 
+      geom_point() +
+      ggtitle("Primerjava BDP in uvoza držav s približno enako površino")+  scale_x_log10() + scale_y_log10() + 
+      theme(axis.text.x=element_blank(),
+            axis.ticks.x=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank()) + ylab("količina v mio $")
+  })
+  output$UPr <- renderPlotly({uvoz.in.bdp.glede.na.prebivalce.shiny %>% filter(leto == input$leto) %>% 
+      ggplot(aes(x=BDP,y=`kolicina_MIO_$`,color=drzava,size=populacija,label=drzava)) + 
+      geom_point() +
+      ggtitle("Primerjava BDP in uvoza držav s približno enako prebivalci")+  scale_x_log10() + scale_y_log10() + 
+      theme(axis.text.x=element_blank(),
+            axis.ticks.x=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank()) + ylab("količina v mio $")
   })
   
-  output$IPr <- renderPlotly({graf.bdp.populacija.izvoz <- ggplot(izvoz.in.bdp.glede.na.prebivalce.1, 
-                                                                  aes(x=BDP,y=`kolicina_MIO_$`,color=drzava,size=populacija,label=drzava)) + 
-    geom_point() + 
-    geom_text(aes(label=drzava),
-              alpha =ifelse(izvoz.in.bdp.glede.na.prebivalce.1$drzava=="Slovenia",1,0),
-              hjust=1, vjust=1.4) +
-    ggtitle("Primerjava BDP in izvoza, držav s približno enako populacijo, leta 2019") +  
-    scale_x_log10() + scale_y_log10() +
-    theme(axis.text.x=element_blank(),
-          axis.ticks.x=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank())
-  
+  output$grupiranje <- renderPlotly({zdruzeno.skupaj2 %>% mutate(skupina=molK$cluster) %>%
+      ggplot(aes(x=Uvoz,y=Izvoz, col=factor(skupina),label=SMTK)) + geom_point(size=3) + 
+      scale_x_log10() + scale_y_log10() + theme(axis.text.x=element_blank(),
+                                                axis.ticks.x=element_blank(),
+                                                axis.text.y=element_blank(),
+                                                axis.ticks.y=element_blank()) +
+      geom_abline(slope=1, intercept=0,linetype="dashed", color = "orange")+
+      ggtitle("Primerjava različnih panog glede na uvoz in izvoz leta 2019")
   })
-  output$UPo <- renderPlotly({ggplot(uvoz.in.bdp.glede.na.povrsino, aes(x=BDP,y=`kolicina_MIO_$`,color=drzava,size=povrsina,label=drzava)) + 
-    geom_point() + geom_text(aes(label=drzava),
-                                                alpha =ifelse(uvoz.in.bdp.glede.na.povrsino$drzava=="Slovenia",1,0),hjust=1, vjust=1.4) +
-    ggtitle("Primerjava BDP in izvoza držav s približno enako površino, leta 2019")+  scale_x_log10() + scale_y_log10() + 
-    theme(axis.text.x=element_blank(),
-          axis.ticks.x=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank())
-  })
-    output$IPo <- renderPlotly({ggplot(izvoz.in.bdp.glede.na.povrsino, aes(x=BDP,y=`kolicina_MIO_$`,color=drzava,size=povrsina,label=drzava)) + 
-    geom_point() + geom_text(aes(label=drzava),
-                                                alpha =ifelse(izvoz.in.bdp.glede.na.povrsino$drzava=="Slovenia",1,0),hjust=1, vjust=1.4) +
-    ggtitle("Primerjava BDP in uvoza držav s približno enako površino, leta 2019") +  scale_x_log10() + scale_y_log10() + 
-    theme(axis.text.x=element_blank(),
-          axis.ticks.x=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank())
   
-    })
-  
-    output$grupiranje <- renderPlotly({zdruzeno.skupaj2 %>% mutate(skupina=molK$cluster) %>%
-        ggplot(aes(x=Uvoz,y=Izvoz, col=factor(skupina),label=SMTK)) + geom_point(size=3) + 
-        scale_x_log10() + scale_y_log10() + theme(axis.text.x=element_blank(),
-                                                  axis.ticks.x=element_blank(),
-                                                  axis.text.y=element_blank(),
-                                                  axis.ticks.y=element_blank()) +
-        geom_abline(slope=1, intercept=0,linetype="dashed", color = "orange")+
-        ggtitle("Primerjava različnih panog glede na uvoz in izvoz leta 2019")
-    })
-  
-  }
+}
 )
-
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,14 +1,71 @@
 library(shiny)
 library(plotly)
 
-
-
-
 shinyUI(fluidPage(
-  plotlyOutput("UPr"),
-  plotlyOutput("IPr"),
-  plotlyOutput("UPo"),
-  plotlyOutput("IPo"),
-  plotlyOutput("grupiranje")
-))
+  navbarPage("Primerjava držav s podobnimi karakteristikami po BDP in trgovanju",
+             
+             tabPanel("Izvoz in BDP glede na površino skozi leta",
+                      titlePanel(title = h2("Izvoz in bdp glede na površino")),
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput(
+                            inputId = "leto",
+                            label = "leto",
+                            choices = c("2010", "2011", "2012","2013","2014","2015","2016","2017",
+                                        "2018","2019")
+                          )
+                        ),
+                        mainPanel(
+                          plotlyOutput(outputId = "IPo")
+                        )
+                      )),
 
+              tabPanel("Izvoz in BDP glede na prebivalce skozi leta",
+                      titlePanel(title = h2("Izvoz in bdp glede na prebivalce")),
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput(
+                          inputId = "leto",
+                          label = "leto",
+                          choices = c("2010", "2011", "2012","2013","2014","2015","2016","2017",
+                                                     "2018","2019")
+                                       )
+                                     ),
+                          mainPanel(
+                          plotlyOutput(outputId = "IPr")
+                                     )
+                                   )),
+
+              tabPanel("Uvoz in BDP glede na površino skozi leta",
+                      titlePanel(title = h2("Uvoz in bdp glede na povrsino")),
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput(
+                          inputId = "leto",
+                          label = "leto",
+                          choices = c("2010", "2011", "2012","2013","2014","2015","2016","2017",
+                                                                  "2018","2019")
+                                  )
+                                  ),
+                          mainPanel(
+                          plotlyOutput(outputId = "UPo")
+                                  )
+                                  )),
+              tabPanel("Uvoz in BDP glede na prebivalce skozi leta",
+                      titlePanel(title = h2("Uvoz in bdp glede na prebivalce")),
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput(
+                          inputId = "leto",
+                          label = "leto",
+                          choices = c("2010", "2011", "2012","2013","2014","2015","2016","2017",
+                                                                               "2018","2019")
+                                  )
+                                  ),
+                          mainPanel(
+                          plotlyOutput(outputId = "UPr")
+                                    ))),
+ 
+            plotlyOutput("grupiranje")                                                            
+            ))
+)
